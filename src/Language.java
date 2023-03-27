b5d417f11869f8f53c6a1151e2e413b8d2357a2d
kenichi beveridge <kenbev84@gmail.com>
2023-03-27 09:22:04 +0100

    added name, numSpeakers, regionsSpoken and wordOrder class variables

--- src/Language.java
+++ src/Language.java
@@ -1,5 +1,10 @@
 public class Language {
 
+    protected String name;
+    protected int numSpeakers;
+    protected String regionsSpoken;
+    protected String wordOrder;
+
     public static void main(String[] args) {
 
     }
